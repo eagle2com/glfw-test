901b7061a0753e9e57ae92c94e4fe7d2f8685e40
eagle2com <peter.lichard@gmail.com>
2013-08-28 18:13:43 +0200

    why is it not exporting :'(

--- GameObject/IGameObject.cpp
+++ GameObject/IGameObject.cpp
@@ -3,130 +3,92 @@
 //#include <glm\glm.hpp>
 
 
-using std::cout;
-using std::endl;
-
-class IGameObject;
-
-struct FunctionHandle
+IGameObject::IGameObject(void) : m_sendevenetptr(nullptr), m_subscribeptr(nullptr)
 {
-	FunctionHandle(int ev, int fn) : event_id(ev), function_id(fn){}
-	int event_id;
-	int function_id;
-};
-
-typedef std::function<bool(void*) > CallbackFn; //a callback function returns a bool to see if the event is "consumed"
-
-typedef void (*SendEventPtr)(unsigned int, void*, bool);
-typedef FunctionHandle* (*SubscribePtr)(std::string, CallbackFn);
-typedef void (*UnSubPtr)(FunctionHandle*) ;
-typedef unsigned int (*GetIdPtr)(std::string);
-typedef void (*SetParamPtr)(std::string, void*);
-typedef void* (*GetParamPtr)(std::string);
-
-
-//game object interface definition
-class IGameObject
+}
+IGameObject::~IGameObject()
 {
-public:
-	IGameObject() : m_sendevenetptr(nullptr), m_subscribeptr(nullptr)
+	cout << "deleted object" << endl;
+	//tell the event manager to erase callbacks
+	for_each(m_functionhandles.begin(), m_functionhandles.end(), [this](FunctionHandle* handle)
 	{
-	}
-	virtual ~IGameObject()
-	{
-		cout << "deleted object" << endl;
-		//tell the event manager to erase callbacks
-		for_each(m_functionhandles.begin(), m_functionhandles.end(), [this](FunctionHandle* handle)
-		{
-			this->UnSub(handle);
-		});
-	}
+		this->UnSub(handle);
+	});
+}
 
-	virtual void Tick() = 0;
-	virtual void Init() = 0;
-
-	void SetFunctions(SendEventPtr send, SubscribePtr sub, UnSubPtr unsub, GetIdPtr idptr)
+void IGameObject::SetFunctions(SendEventPtr send, SubscribePtr sub, UnSubPtr unsub, GetIdPtr idptr)
+{
+	m_sendevenetptr = send;
+	m_subscribeptr = sub;
+	m_unsubptr = unsub;
+	m_getidptr = idptr;
+}
+void IGameObject::Subscribe(std::string name, CallbackFn fn)
+{
+	if (m_subscribeptr)
 	{
-		m_sendevenetptr = send;
-		m_subscribeptr = sub;
-		m_unsubptr = unsub;
-		m_getidptr = idptr;
+		FunctionHandle* handle = m_subscribeptr(name, fn);
+		m_functionhandles.push_back(handle);
 	}
-	void Subscribe(std::string name, CallbackFn fn)
-	{
-		if (m_subscribeptr)
-		{
-			FunctionHandle* handle = m_subscribeptr(name, fn);
-			m_functionhandles.push_back(handle);
-		}
-		else
-			throw;
-	}
+	else
+		throw;
+}
 
-	void UnSub(FunctionHandle* handle)
-	{
-		if (m_unsubptr)
-			m_unsubptr(handle);
-		else
-			throw;
-	}
+void IGameObject::UnSub(FunctionHandle* handle)
+{
+	if (m_unsubptr)
+		m_unsubptr(handle);
+	else
+		throw;
+}
 
-	unsigned int GetID(std::string name)
+unsigned int IGameObject::GetID(std::string name)
+{
+	if (m_getidptr)
 	{
-		if (m_getidptr)
-		{
-			unsigned int id = m_getidptr(name);
-			//cout << "[object].GetID: "<<id<<endl;
-			return id;
-		}
-		else
-			throw;
+		unsigned int id = m_getidptr(name);
+		//cout << "[object].GetID: "<<id<<endl;
+		return id;
 	}
+	else
+		throw;
+}
 
-	void SendEvent(unsigned int ID, void* data, bool free_data = false)
+void IGameObject::SendEvent(unsigned int ID, void* data, bool free_data = false)
+{
+	if (m_sendevenetptr)
 	{
-		if (m_sendevenetptr)
-		{
-			//cout << "[object].SendEvent: "<<ID<<endl;
-			m_sendevenetptr(ID, data, free_data);
-			return;
-		}
-		else
-			throw;
+		//cout << "[object].SendEvent: "<<ID<<endl;
+		m_sendevenetptr(ID, data, free_data);
+		return;
 	}
+	else
+		throw;
+}
 
-	void Destroy()
+void IGameObject::Destroy()
+{
+	delete this;
+}
+
+void* IGameObject::GetParam(std::string name)
+{
+	std::map<std::string, CParam*>::iterator it = m_params.find(name);
+	if (it != m_params.end())
+		return it->second->m_data;
+	else
+		return NULL;
+}
+
+void IGameObject::SetParam(std::string name, void* p)
+{
+	std::map<std::string, CParam*>::iterator it = m_params.find(name);
+	if (it != m_params.end())
 	{
-		delete this;
+		delete it->second->m_data;
+		it->second->m_data = p;
 	}
-
-	void* GetParam(std::string name)
-	{
-		std::map<std::string, CParam*>::iterator it = m_params.find(name);
-		if (it != m_params.end())
-			return it->second->m_data;
-		else
-			return NULL;
-	}
-
-	void SetParam(std::string name, void* p)
-	{
-		std::map<std::string, CParam*>::iterator it = m_params.find(name);
-		if (it != m_params.end())
-		{
-			delete it->second->m_data;
-			it->second->m_data = p;
-		}
-		else
-			m_params[name] = new CParam(name, p);
-	}
-
-private:
-	std::map<std::string, CParam*> m_params;
-	std::vector<FunctionHandle*> m_functionhandles; //vector holding all the handles we have to remove from the event manager after destroying the object
-	SendEventPtr m_sendevenetptr;
-	SubscribePtr m_subscribeptr;
-	UnSubPtr m_unsubptr;
-	GetIdPtr m_getidptr;
-};
+	else
+		m_params[name] = new CParam(name, p);
+}
 
